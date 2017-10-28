063a0fb5f8cedf46a2e0cb1b64d1de7148d8d2c8
itanony <68748016@qq.com>
2017-10-28 22:54:08 +0800

    20171028-第一版login.py

--- module1/login.py
+++ module1/login.py
@@ -1,4 +1,15 @@
 #/usr/bin/env python
 #coding=utf8
 #Author: itanony
-print ("Hello World!")
+
+#20171028第一版本,
+n = 1
+while n <= 3:
+    user_name = raw_input("Please Input UserName:")
+    password = raw_input("Please Input Password:")
+    if user_name == 'admin' and password == 'admin':
+        print ("welcome! now ,do something!")
+        break
+    else:
+        print ("用户名密码错误，请重新输入\n你还有%s次机会") %(3-n)
+        n += 1
