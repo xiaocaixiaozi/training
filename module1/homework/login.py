f6fcc1577da45e73c0db135eb1959e5e43a315b4
itanony <68748016@qq.com>
2017-10-30 23:26:24 +0800

    修改判断passwd文件，badlist文件为空，不存在时的异常

--- module1/homework/login.py
+++ module1/homework/login.py
@@ -13,7 +13,7 @@
 #     else:
 #         print ("用户名密码错误，请重新输入\n你还有%s次机会") %(3-n)
 #         n += 1
-#201710209第二版本
+#201710209第二版本-----这一版本就是堆狗屎
 # import os
 # import getpass
 # n = 0
@@ -50,26 +50,34 @@
 import os
 import sys
 while True:
-    username=input("username:")
+    username=input("username: ")
     dict={}
-    with open("passwd.csv","r") as f:
-        for line in f.readlines():
-            dict[line.split(",")[0]]=line.split(",")[1].strip("\n")
-        if username not in dict.keys():
-            print("用户名不存在")
-            continue
-    with open("badlist","r") as f:
-        if username in [x.strip(os.linesep) for x in f.readlines()]:
-            print("user locked")
-            continue
+    if os.path.exists("passwd.csv"):
+        if os.path.getsize("passwd.csv"):
+            with open("passwd.csv","r") as f:
+                for line in f.readlines():
+                    dict[line.split(",")[0]]=line.split(",")[1].strip("\n")
+                if username not in dict.keys():
+                    print("unknown username")
+                    continue
+        else:
+            print("passwd file isempty！")
+    else:
+        print("passwd file not found")
+        sys.exit(1)
+    if os.path.exists("badlist"):
+        with open("badlist","r") as f:
+            if username in [x.strip(os.linesep) for x in f.readlines()]:
+                print("user locked")
+                continue
     n = 0
-    while n<3:
+    while n < 3:
         password=input("password: ")
         if dict[username]==password:
             print("welcome man!")
             sys.exit(0)
         else:
-            print("密码错误！")
+            print("invalid password！")
             n += 1
     else:
         print("bye bye!")
