b1844b4a8fbb8dff6702d96a5053965e073efffe
itanony <68748016@qq.com>
2017-11-19 16:06:45 +0800

    工资管理系统第二版

--- module1/homework/salary.py
+++ module1/homework/salary.py
@@ -1,66 +1,59 @@
-#/usr/bin/env python
-#coding=utf8
-#Author: itanony
+# /usr/bin/env python
+# coding=utf8
+# Author: itanony
 
 db = 'info.txt'
-funcs = ['查询',
-         '修改',
-         '增加',
-         '退出']
+
+
 def user_exit(name):
-    with open(db,'r') as f:
+    with open(db, 'r') as f:
         for line in f.readlines():
             if name == line.split(" ")[0]:
                 return True
 
+
+def search(username, ):
+    if user_exit(username):
+        with open(db, 'r') as f:
+            for line in f.readlines():
+                if username in line:
+                    print(line.split(' ')[1])
+
+
+def modify(username, salary=''):
+    if user_exit(username):
+        new_salary = input("Please input salary:")
+        with open(db, "r") as f:
+            lines = f.readlines()
+        with open(db, "w") as f:
+            for line in lines:
+                if username in line:
+                    line = "%s %s\n" % (username, new_salary)
+                f.write(line)
+        print("修改成功")
+
+
+def add(username, salary=''):
+    if not user_exit(username):
+        salary = input("Please input salary:")
+        with open(db, 'a') as f:
+            f.write("%s %s\n" % (username, salary))
+        print("添加成功")
+
+func_dict = {
+    "查询用户": search,
+    "修改用户": modify,
+    "添加用户": add,
+}
+
 while 1:
-    for x in range(len(funcs)):
-        print ("%s.%s" %(x,funcs[x]))
-    choice = input(">>>")
-    if choice.isdigit():
-        choice = int(choice)
-        if choice == 0:
-            username = input("username:")
-            if user_exit(username):
-                with open(db,mode='r') as f:
-                    for line in f:
-                        if username in line:
-                            print(line.split(" ")[1])
-            else:
-                print("no such user")
-        elif choice == 1:
-            username = input("username:")
-            if user_exit(username):
-                salary = input("new salary:")
-                with open(db,"r") as f:
-                    lines = f.readlines()
-                with open(db,"w") as f:
-                    for line in lines:
-                        if username in line:
-                            line = "%s %s\n" %(username,salary)
-                        f.write(line)
-                print("修改成功")
-            else:
-                print("no such user")
-
-        elif choice == 2:
-            username = input("username")
-            if not user_exit(username):
-                salary = input("salary:")
-                with open(db,"a") as f:
-                    line = "%s %s\n" %(username,salary)
-                    f.write(line)
-                print("添加成功")
-            else:
-                print("用户已经存在")
-
-        elif choice == 3:
-            print("再见")
-            break
+    for index,item in enumerate(func_dict.keys()):
+        print(index,item)
+    choice = input("Please Input Your Choice,[q] for exit>> ")
+    if choice in list(func_dict.keys()):
+        username = input("请输入用户名>> ")
+        func_dict[choice](username)
+    elif choice == 'q':
+        break
     else:
-        print("傻逼！重新输入")
-
-
-
-
-
+        print("输入有误,重新输入")
