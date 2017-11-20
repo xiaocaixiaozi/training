836709a645bb8d325db3c2d8608bad732d6cfc89
anonxiaozi <bloke_anon@126.com>
2017-11-20 17:28:54 +0800

    创建员工信息表

---
+++ module2/员工信息表/main.py
@@ -0,0 +1,86 @@
+# /usr/bin/env python
+# coding=utf8
+# Author: itanony
+
+import sqlite3
+
+db = 'staff'
+create_table_sql = '''Create table %s (
+staff_id int auto_increment not null,
+name varchar(30) not null,
+age char(3) not null,
+phone char(20) primary key not null,
+enroll_date char(10) not null,
+)
+'''
+
+def conn_db(db_name):
+    conn = sqlite3.conn(db_name)
+    cursor = conn.cursor()
+    return conn, cursor
+
+def close_db(conn, cursor):
+    cursor.close()
+    conn.close()
+
+def create_table(db_name, command):
+    conn, cursor = conn_db(db_name)
+    cursor.execute(command)
+    close_db(conn, cursor)
+
+def user_exit(name):
+    with open(db, 'r') as f:
+        for line in f.readlines():
+            if name == line.split(" ")[0]:
+                return True
+
+def search(username, ):
+    if user_exit(username):
+        with open(db, 'r') as f:
+            for line in f.readlines():
+                if username in line:
+                    print(line.split(' ')[1])
+    else:
+        print("用户不存在")
+
+def modify(username, salary=''):
+    if user_exit(username):
+        new_salary = input("Please input salary:")
+        with open(db, "r") as f:
+            lines = f.readlines()
+        with open(db, "w") as f:
+            for line in lines:
+                if username in line:
+                    line = "%s %s\n" % (username, new_salary)
+                f.write(line)
+        print("修改成功")
+    else:
+        print("用户不存在")
+
+def add(username, salary=''):
+    if not user_exit(username):
+        salary = input("Please input salary:")
+        with open(db, 'a', encoding='utf8') as f:
+            f.write("%s %s\n" % (username, salary))
+        print("添加成功")
+    else:
+        print("用户已经存在")
+
+func_dict = {
+    "查询用户": search,
+    "修改用户": modify,
+    "添加用户": add,
+}
+
+while 1:
+    for index,item in enumerate(func_dict.keys()):
+        print(index,item)
+    choice = input("Please Input Your Choice,[q] for exit>> ")
+    if choice in list(func_dict.keys()):
+        username = input("请输入用户名>> ")
+        func_dict[choice](username)
+    elif choice == 'q':
+        break
+    else:
+        print("输入有误,重新输入")
+
