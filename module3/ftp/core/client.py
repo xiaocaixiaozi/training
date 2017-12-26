df8e2d3a6c164c599acad8fbbd3d1acb5a510442
xiaocaixiaozi <68748016@qq.com>
2017-12-26 22:32:53 +0800

    需要修改上传下载文件

--- module3/ftp/core/client.py
+++ module3/ftp/core/client.py
@@ -3,19 +3,55 @@
 # Author: bloke
 
 import socket
+import os
+import json
 
 host, port = 'localhost', 9999
+transfer_size = 4096
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     client.connect((host, port))
+    welcome = client.recv(transfer_size)  # 欢迎语
+    print(welcome.decode('utf-8'))
     while 1:
         data = input('Send: ').strip()
         if not data: continue
-        client.sendall(bytes(data, 'utf-8'))
-        recv = client.recv(1024)
-        print('Recv:', recv)
+        client.sendall(bytes(str(len(data)), encoding='utf-8'))
+        if data == 'help':
+            client.sendall(data.encode('utf-8'))
+            recv_data = client.recv(transfer_size)
+            recv_data = json.loads(recv_data.decode('utf-8'))
+            print(''.center(50, '*'))
+            for key, value in recv_data.items():
+                print(key, ': ', value)
+            print(''.center(50, '*'))
+            continue
 
+        if data.split()[0] == 'put':    # 上传文件
+            action, filename = data.split()[0], data.split()[1]
+            filesize = os.path.getsize(filename)
+            client.sendall(bytes('%s %s %s' % (action, filename, filesize), encoding='utf-8'))
+            with open(filename, 'rb') as f:
+                for line in f:
+                    client.sendall(line)
 
+        elif data.split()[0] == 'get':  # 下载文件
+            filecount = 0
+            action, filename = data.split()[0], os.path.basename(data.split()[1])
+            client.sendall(data.encode('utf-8'))
+            filesize = client.recv(transfer_size)
+            with open(filename, 'wb') as f:
+                while filecount < int(filesize):
+                    recv_data = client.recv(transfer_size)
+                    f.write(recv_data)
+            continue
+
+        else:
+            client.sendall(bytes(data, 'utf-8'))
+            print('Recv:')
+            recv_data = client.recv(transfer_size)
+            print(recv_data.decode('utf-8'))
+            continue
 
 
 
--- module3/ftp/core/server.py
+++ module3/ftp/core/server.py
@@ -3,26 +3,79 @@
 # Author: bloke
 
 import socketserver
+import subprocess
+import os
+import json
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
+    transfer_count = 4096  # 全局变量，网络传输大小，字节
+
+    def myinit(self):
+        self.comm_dict = {
+            'cd': 'Change Dir',
+            'dir': 'List Dir',
+            'get': 'Download File',
+            'put': 'Upload File'
+        }
+        self.error_dict = {
+            'comm_error': 'Invalid Commands',
+        }
+
     def setup(self):
+        self.myinit()
         print('[%s:%s] is connected.' % self.client_address)
+        self.request.sendall(bytes('Welcome %s, Please enter "help" to help.' % str(self.client_address), \
+                                   encoding='utf-8'))
 
     def handle(self):
-        while 1:
-            try:
-                self.data = self.request.recv(1)
-            except ConnectionResetError as e:
-                break
-            if not self.data:
-                break
-            print('Recv:', self.data)
-            self.request.sendall(self.data.upper())
+        try:
+            while 1:
+                count = 0
+                recv_list = []
+                num = self.request.recv(self.transfer_count)
+                while count < int(num.decode('utf-8')):
+                    recv_data = self.request.recv(self.transfer_count).decode('utf-8')
+                    recv_list.append(recv_data)
+                    count += self.transfer_count
+                else:
+                    recv = ''.join(recv_list)
+                    print('Recv:', recv)
+                    if recv == 'help':
+                        send_data = json.dumps(self.comm_dict)
+                        self.request.sendall(bytes(send_data, encoding='utf-8'))
+                        continue
+                    if recv.split()[0] == 'put':
+                        action, filename, filesize = recv.split()[0], recv.split()[1], recv.split()[2]
+                        self.transfer_file(action, filename, filesize)
+                        continue
+                    elif recv.split()[0] == 'get':
+                        action, filename = recv.split()[0], recv.split()[1]
+                        filesize = os.path.getsize(filename)
+                        self.request.send(bytes(str(filesize), encoding='utf-8'))
+                        self.transfer_file(action, filename, filesize)
+                        continue
+
+                    data = subprocess.getoutput(recv)
+                    self.request.sendall(bytes(data, encoding='utf-8'))
+        except ConnectionResetError as e:
+            print(e)
 
     def finish(self):
         print('[%s:%s] is disconnected.' % self.client_address)
+
+    def transfer_file(self, action, filename, filesize):
+        filecount = 0
+        if action == 'put':
+            with open(filename, 'wb') as f:
+                while filecount < filesize:
+                    recv_data = self.request.recv(self.transfer_count)
+                    f.write(recv_data)
+        elif action == 'get':
+            with open(filename, 'rb') as f:
+                for line in f:
+                    self.request.sendall(line)
 
 
 if __name__ == "__main__":
